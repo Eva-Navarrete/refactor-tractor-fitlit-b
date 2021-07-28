22d0acf2d5554e4682f50dae76b09490fe5a22e4
Hayley Dixon <hdixon37@gmail.com>
2021-07-27 20:25:43 -0600

    Clean class and tests

--- src/User-repo.js
+++ src/User-repo.js
@@ -1,30 +1,41 @@
+import 'dayjs';
 class UserRepo {
   constructor(users) {
     this.users = users;
   }
+
   getDataFromID(id) {
-    return this.users.find((user) => id === user.id);
+    let user = this.users.find((user) => id === user.id);
+    return user;
   }
+// is this necessary here?
   getDataFromUserID(id, dataSet) {
+    // console.log(dataSet.filter((userData) => id === userData.userID))
     return dataSet.filter((userData) => id === userData.userID);
   }
+
   calculateAverageStepGoal() {
-    var totalStepGoal = this.users.reduce((sumSoFar, data) => {
-      return sumSoFar = sumSoFar + data.dailyStepGoal;
+    let totalStepGoal = this.users.reduce((sum, steps) => {
+      sum = sum + steps.dailyStepGoal
+      return sum;
     }, 0);
     return totalStepGoal / this.users.length;
   }
-  makeSortedUserArray(id, dataSet) {
+// sorting the arry from least to highest to find the lowest date (ie today) (getToday())
+  makeSortedUserArray(id, dataSet) { // should be set on each class, not here.
     let selectedID = this.getDataFromUserID(id, dataSet)
     let sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedByDate;
   }
+
   getToday(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet)[0].date;
   }
+
   getFirstWeek(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet).slice(0, 7);
   }
+
   getWeekFromDate(date, id, dataSet) {
     let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date)));
     return this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
@@ -34,11 +45,13 @@ class UserRepo {
       return (new Date(date)).setDate((new Date(date)).getDate() - 7) <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date)
     })
   }
+
   chooseDayDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
       return dataItem.date === date
     });
   }
+
   isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod) {
     return listFromMethod.reduce(function(objectSoFar, dataItem) {
       if (!objectSoFar[dataItem.userID]) {
@@ -49,6 +62,7 @@ class UserRepo {
       return objectSoFar;
     }, {});
   }
+
   rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     return Object.keys(sortedObjectKeys).sort(function(b, a) {
@@ -61,6 +75,7 @@ class UserRepo {
       }, 0) / sortedObjectKeys[b].length)
     });
   }
+
   combineRankedUserIDsAndAveragedData(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     let rankedUsersAndAverages = this.rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod)
