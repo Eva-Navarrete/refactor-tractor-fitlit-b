f802615bcb850a8f704d5884fe097ad7a4558166
Hayley Dixon <hdixon37@gmail.com>
2021-07-27 20:26:01 -0600

    Organize code and connect the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -64,39 +64,40 @@ function startApp() {
   let hydrationRepo = new Hydration(hydrationData);
   let sleepRepo = new Sleep(sleepData);
   let activityRepo = new Activity(activityData);
-  let userNowId = pickUser(); // choices random user
-  let userNow = getUserById(userNowId, userRepo);
-  let today = makeToday(userRepo, userNowId, hydrationData);
-  let randomHistory = makeRandomDate(userRepo, userNowId, hydrationData);
+  let currentUserID = randomizeId(); // choices random user
+  let currentUser = getUserById(currentUserID, userRepo); // user object
+
+  let today = makeToday(userRepo, currentUserID, hydrationData); // Here they are only finding the today based on hydrationData!
+  let randomHistory = makeRandomDate(userRepo, currentUserID, hydrationData);
 
   historicalWeek.forEach((instance) =>
     instance.insertAdjacentHTML('afterBegin', `Week of ${randomHistory}`)
   );
-  addInfoToSidebar(userNow, userRepo);
-  addHydrationInfo(userNowId, hydrationRepo, today, userRepo, randomHistory);
-  addSleepInfo(userNowId, sleepRepo, today, userRepo, randomHistory);
-  let winnerNow = makeWinnerID(activityRepo, userNow, today, userRepo);
-  addActivityInfo(
-    userNowId,
+  renderSidebar(currentUser, userRepo);
+  addHydrationInfo(currentUserID, hydrationRepo, today, userRepo, randomHistory);
+  renderSleep(currentUserID, sleepRepo, today, userRepo, randomHistory);
+  let winnerNow = makeWinnerID(activityRepo, currentUser, today, userRepo);
+  renderActivity(
+    currentUserID,
     activityRepo,
     today,
     userRepo,
     randomHistory,
-    userNow,
+    currentUser,
     winnerNow
   );
-  addFriendGameInfo(
-    userNowId,
+  renderFriendGame(
+    currentUserID,
     activityRepo,
     userRepo,
     today,
     randomHistory,
-    userNow
+    currentUser
   );
 }
 
 // instantiates an array of user class objects
-function instantiateUsers(array) {
+function instantiateUsers(array) { // REFACTOR!!
   userData.forEach(function (dataItem) {
     let user = new User(dataItem);
     array.push(user);
@@ -105,7 +106,7 @@ function instantiateUsers(array) {
 }
 
 // Function that picks user to display
-function pickUser() {
+function randomizeId() {
   return Math.floor(Math.random() * 50);
 }
 
@@ -114,8 +115,21 @@ function getUserById(id, listRepo) {
   return listRepo.getDataFromID(id);
 }
 
+const renderPage() => {
+
+
+
+
+
+
+
+
+
+
+}
+
 //DOM Manipulation Functions
-function addInfoToSidebar(user, userStorage) {
+function renderSidebar(user, userStorage) {
   sidebarName.innerText = user.name;
   headerText.innerText = `${user.getFirstName()}'s Activity Tracker`;
   stepGoalCard.innerText = `Your daily step goal is ${user.dailyStepGoal}.`;
@@ -125,12 +139,15 @@ function addInfoToSidebar(user, userStorage) {
   userStridelength.innerText = `Your stridelength is ${user.strideLength} meters.`;
   friendList.insertAdjacentHTML(
     'afterBegin',
-    makeFriendHTML(user, userStorage)
+    renderFriendList(user, userStorage)
   );
 }
 
-function makeFriendHTML(user, userStorage) {
-  return user
+
+
+
+function renderFriendList(user, userStorage) { // this is rendered inside of renderSidebar
+  return user // return is happening prior to the function? No... Return is holding the function
     .getFriendsNames(userStorage)
     .map(
       (friendName) => `<li class='historical-list-listItem'>${friendName}</li>`
@@ -138,21 +155,21 @@ function makeFriendHTML(user, userStorage) {
     .join('');
 }
 
-function makeWinnerID(activityInfo, user, dateString, userStorage) {
+function makeWinnerID(activityInfo, user, dateString, userStorage) { // NOT DOM MANI
   return activityInfo.getWinnerId(user, dateString, userStorage);
 }
 
-function makeToday(userStorage, id, dataSet) {
-  var sortedArray = userStorage.makeSortedUserArray(id, dataSet);
+function makeToday(userStorage, id, dataSet) { // NOT DOM
+  let sortedArray = userStorage.makeSortedUserArray(id, dataSet);
   return sortedArray[0].date;
 }
 
-function makeRandomDate(userStorage, id, dataSet) {
-  var sortedArray = userStorage.makeSortedUserArray(id, dataSet);
+function makeRandomDate(userStorage, id, dataSet) { // NOT DOM
+  var sortedArray = userStorage.makeSortedUserArray(id, dataSet); // using a method on the userStorage?
   return sortedArray[Math.floor(Math.random() * sortedArray.length + 1)].date;
 }
 
-function addHydrationInfo(
+function addHydrationInfo( // Dom manipulation
   id,
   hydrationInfo,
   dateString,
@@ -192,7 +209,7 @@ function addHydrationInfo(
   );
 }
 
-function makeHydrationHTML(id, hydrationInfo, userStorage, method) {
+function makeHydrationHTML(id, hydrationInfo, userStorage, method) { // dom monipulation
   return method
     .map(
       (drinkData) =>
@@ -201,7 +218,7 @@ function makeHydrationHTML(id, hydrationInfo, userStorage, method) {
     .join('');
 }
 
-function addSleepInfo(id, sleepInfo, dateString, userStorage, laterDateString) {
+function renderSleep(id, sleepInfo, dateString, userStorage, laterDateString) { //
   sleepToday.insertAdjacentHTML(
     'afterBegin',
     `<p>You slept</p> <p><span class="number">${sleepInfo.calculateDailySleep(
@@ -260,7 +277,7 @@ function makeSleepQualityHTML(id, sleepInfo, userStorage, method) {
     .join('');
 }
 
-function addActivityInfo(
+function renderActivity(
   id,
   activityInfo,
   dateString,
@@ -345,7 +362,7 @@ function addActivityInfo(
   );
   userMinutesThisWeek.insertAdjacentHTML(
     'afterBegin',
-    makeMinutesHTML(
+    renderMinutes(
       id,
       activityInfo,
       userStorage,
@@ -385,7 +402,7 @@ function makeStairsHTML(id, activityInfo, userStorage, method) {
     .join('');
 }
 
-function makeMinutesHTML(id, activityInfo, userStorage, method) {
+function renderMinutes(id, activityInfo, userStorage, method) { // invoked inside of renderActivity function
   return method
     .map(
       (data) => `<li class="historical-list-listItem">On ${data} minutes</li>`
@@ -393,7 +410,7 @@ function makeMinutesHTML(id, activityInfo, userStorage, method) {
     .join('');
 }
 
-function addFriendGameInfo(
+function renderFriendGame( // Dom Manipulation
   id,
   activityInfo,
   userStorage,
